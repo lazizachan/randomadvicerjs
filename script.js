16acacc3d3606c118a8b5ff73f61147912e5fd03
lazizachan <lazizaakhadova@gmail.com>
2025-01-26 16:47:31 +0500

    create some simple random advicer

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+window.addEventListener("load", () => {
+  document.querySelector(".loading").style.display = "flex";
+  setTimeout(() => {
+    document.querySelector(".loading").style.display = "none";
+    document.querySelector(".advice_box").style.display = "flex";
+    document.querySelector(".advice_box").style.opacity = "1";
+  }, 5000);
+
+  document.querySelector(".advice_box").style.display = "block";
+});
+const advice = document.querySelector(".advice");
+const btnCont = document.querySelector(".btn-cont");
+
+btnCont.addEventListener("click", () => {
+  btnCont.disabled = true;
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => (advice.textContent = data.slip.advice));
+
+  setTimeout(() => {
+    btnCont.disabled = false;
+  }, 3000);
+});
